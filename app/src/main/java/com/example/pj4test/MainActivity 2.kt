9d44087d2a0aeddaf8ea7f31fc0dc26c3e4208e7
jmink1896 <jmink1896@gmail.com>
2023-05-31 22:23:02 +0900

    naive control flow

--- app/src/main/java/com/example/pj4test/MainActivity 2.kt
+++ app/src/main/java/com/example/pj4test/MainActivity 2.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
 
         checkPermissions() // check permissions
     }
--- app/src/main/java/com/example/pj4test/fragment/AudioFragment 2.kt
+++ app/src/main/java/com/example/pj4test/fragment/AudioFragment 2.kt
@@ -11,28 +11,26 @@ import com.example.pj4test.ProjectConfiguration
 import com.example.pj4test.audioInference.SnapClassifier
 import com.example.pj4test.databinding.FragmentAudioBinding
 
-
-
 class AudioFragment: Fragment(), SnapClassifier.DetectorListener {
     private val TAG = "AudioFragment"
+
     private var _fragmentAudioBinding: FragmentAudioBinding? = null
 
-
-    companion object {
-        // classifiers
-        lateinit var snapClassifier: SnapClassifier
-
-        var squatting = 0
-        fun startInferencing() {
-            // Code for the static method
-            snapClassifier.startInferencing()
-        }
-    }
     private val fragmentAudioBinding
         get() = _fragmentAudioBinding!!
 
+//    // classifiers
+//    lateinit var snapClassifier: SnapClassifier
 
+    companion object {
+        var state: Int = 0
+        // classifiers
+        lateinit var snapClassifier: SnapClassifier
+        fun startInferencing() {
+            // Code for staticMethod2
+        }
 
+    }
     // views
     lateinit var snapView: TextView
 
@@ -54,8 +52,6 @@ class AudioFragment: Fragment(), SnapClassifier.DetectorListener {
         snapClassifier = SnapClassifier()
         snapClassifier.initialize(requireContext())
         snapClassifier.setDetectorListener(this)
-        Log.d(TAG, "onViewCreated")
-
     }
 
     override fun onPause() {
@@ -70,7 +66,7 @@ class AudioFragment: Fragment(), SnapClassifier.DetectorListener {
 
     override fun onResults(score: Float) {
 //        TODO: start filming here when clap detected
-//        activity?.runOnUiThread {
+        activity?.runOnUiThread {
 //            if (score > SnapClassifier.THRESHOLD) {
 //                snapView.text = "SNAP"
 //                snapView.setBackgroundColor(ProjectConfiguration.activeBackgroundColor)
@@ -80,20 +76,20 @@ class AudioFragment: Fragment(), SnapClassifier.DetectorListener {
 //                snapView.setBackgroundColor(ProjectConfiguration.idleBackgroundColor)
 //                snapView.setTextColor(ProjectConfiguration.idleTextColor)
 //            }
-//        }
-        activity?.runOnUiThread {
-            if (squatting == 0 && score > SnapClassifier.THRESHOLD) {
+            if (state == 0 && score > SnapClassifier.THRESHOLD) {
+                Log.d(TAG, "snap detected")
+                state = 1
                 snapView.text = "RECORDING"
                 snapView.setBackgroundColor(ProjectConfiguration.activeBackgroundColor)
                 snapView.setTextColor(ProjectConfiguration.activeTextColor)
-                squatting = 1
-                Log.d(TAG, "recording started")
-
-                snapClassifier.stopInferencing()
+//                snapClassifier.stopInferencing()
             }
-
-
-
+            else if (state == 0 && score < SnapClassifier.THRESHOLD) {
+//                Log.d(TAG, "snap detected")
+                snapView.text = "NOT RECORDING"
+                snapView.setBackgroundColor(ProjectConfiguration.idleBackgroundColor)
+                snapView.setTextColor(ProjectConfiguration.idleTextColor)
+            }
         }
     }
 }
